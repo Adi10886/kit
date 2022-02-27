347dffbdb9e129e6d52470287297aed464fc174f
Adi10886 <77274787+Adi10886@users.noreply.github.com>
2022-02-27 13:55:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+for(var i = 0; i<numberOfDrumButton; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                   
+                var tom1 = new Audio("tom-1.mp3");
+                tom1.play();
+   
+                
+                
+                break;
+            case "a":
+                   
+               var tom2 = new Audio("tom-2.mp3");
+               tom2.play();
+
+               break;
+            case "s":
+                   
+             
+                   
+               var tom3 = new Audio("tom-3.mp3");
+               tom3.play();
+                  
+               break;
+               case "d":
+                      
+                  var tom4 = new Audio("tom-4.mp3");
+                  tom4.play();
+                     
+                break;
+                case "j":
+                       
+                   var crash = new Audio("crash.mp3");
+                   crash.play();
+                      
+                break;
+                case "k":
+                       
+                   var snare = new Audio("snare.mp3");
+                   snare.play();
+                      
+                break;
+                case "l":
+                       
+                   var kick = new Audio("kick-bass.mp3");
+                   kick.play();
+    
+    
+    
+   
+
+            
+
+            
+       
+        
+            default: console.log(buttonInnerHTML);
+               
+        }
+        
+
+     
+
+
+   
+    });
+
+}    
+
+
+
+
+
+
+
+
+
+
