c4a8de93bfb5010d02182731e4ad8eef4972b8c3
Adi10886 <77274787+Adi10886@users.noreply.github.com>
2022-02-27 20:15:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,17 @@
 
+
+//detecting button press
+
+
 var numberOfDrumButton = document.querySelectorAll(".drum").length;
 for(var i = 0; i<numberOfDrumButton; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
         var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
 
-        switch (buttonInnerHTML) {
-            case "w":
-                   
-                var tom1 = new Audio("tom-1.mp3");
-                tom1.play();
-   
-                
-                
-                break;
-            case "a":
-                   
-               var tom2 = new Audio("tom-2.mp3");
-               tom2.play();
-
-               break;
-            case "s":
-                   
-             
-                   
-               var tom3 = new Audio("tom-3.mp3");
-               tom3.play();
-                  
-               break;
-               case "d":
-                      
-                  var tom4 = new Audio("tom-4.mp3");
-                  tom4.play();
-                     
-                break;
-                case "j":
-                       
-                   var crash = new Audio("crash.mp3");
-                   crash.play();
-                      
-                break;
-                case "k":
-                       
-                   var snare = new Audio("snare.mp3");
-                   snare.play();
-                      
-                break;
-                case "l":
-                       
-                   var kick = new Audio("kick-bass.mp3");
-                   kick.play();
-    
-    
-    
-   
-
-            
-
-            
        
-        
-            default: console.log(buttonInnerHTML);
-               
-        }
-        
 
      
 
@@ -73,6 +20,77 @@ for(var i = 0; i<numberOfDrumButton; i++) {
     });
 
 }    
+
+
+//detecting keyboard press
+
+document.addEventListener("keypress", function(event) {  
+    
+    makeSound(event.key); 
+});
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+               
+            var tom1 = new Audio("tom-1.mp3");
+            tom1.play();
+
+            
+            
+            break;
+        case "a":
+               
+           var tom2 = new Audio("tom-2.mp3");
+           tom2.play();
+
+           break;
+        case "s":
+               
+         
+               
+           var tom3 = new Audio("tom-3.mp3");
+           tom3.play();
+              
+           break;
+           case "d":
+                  
+              var tom4 = new Audio("tom-4.mp3");
+              tom4.play();
+                 
+            break;
+            case "j":
+                   
+               var crash = new Audio("crash.mp3");
+               crash.play();
+                  
+            break;
+            case "k":
+                   
+               var snare = new Audio("snare.mp3");
+               snare.play();
+                  
+            break;
+            case "l":
+                   
+               var kick = new Audio("kick-bass.mp3");
+               kick.play();
+
+
+
+
+
+        
+
+        
+   
+    
+        default: console.log(buttonInnerHTML);
+           
+    }
+    
+    
+}
 
 
 
